ced6bd0968053e7dd68ffe043d634075ab531d96
annt23 <an12ck2@gmail.com>
2016-07-17 16:04:28 +0700

    detail_course.json

--- codeschool/js/app/tutorial.js
+++ codeschool/js/app/tutorial.js
@@ -1,10 +1,37 @@
 (function() {
 	'use strict';
 
-	angular.module('eduCore').controller('eduTutorialController', ['$scope', eduTutorialController]);
+	angular.module('eduCore').factory('eduCourseFactory', ['$http', eduCourseFactory]);
 
-	function eduTutorialController($scope) {
-		console.log('eduTutorialController');
+	function eduCourseFactory($http) {
+		function getListDetailCourse(callback) {
+			$http.get('../codeschool/data/detail_course.json').then(function(response) {
+				callback(null, response.data);
+			}, function(error) {
+				callback(error, null);
+			});
+		}
+
+		return {
+			list : getListDetailCourse
+		};
+	}
+
+
+	angular.module('eduCore').controller('eduTutorialController', ['$scope', 'eduCourseFactory', eduTutorialController]);
+
+	function eduTutorialController($scope, eduCourseFactory) {
+		$scope.listCourse = [];
+		
+		eduCourseFactory.list(listCourseCallback);
+		function listCourseCallback(error, dataCallback) {
+			if (!error) {
+				$scope.listCourse = dataCallback.data;
+			} else {
+				console.log(error);
+			}
+		}
+
 	}
 
 })();
